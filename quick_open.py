50fcd6f5d58bd517decb4f8efcac8b6a9f8aa95e
Alec Lee <alec.lee15@gmail.com>
2012-03-20 14:52:39 -0400

    functions better with command pallette

--- quick_open.py
+++ quick_open.py
@@ -1,43 +1,31 @@
 import sublime, sublime_plugin
+import sys
 import os
 
 class QuickOpenCommand(sublime_plugin.WindowCommand):
-
-    def run(self):
-        self.options = ["Edit file", "Change directory", "Set working directory"]
-        self.show_commands()
-
-    def show_commands(self):
-        self.window.show_quick_panel(self.options, self.choose_command, sublime.MONOSPACE_FONT)
-
-
-    def choose_command(self, call_value):
-        self.command = self.options[call_value]
-        if self.command == "Change directory":
-            self.change_directory()
-        elif self.command == "Edit file":
+    def run(self, command):
+        if command == "open":
             self.open_new_file()
-        else:
+        elif command == "cd":
+            self.change_directory()
+        elif command == "setdir":
             self.set_working_directory()
-
 
     def change_directory(self):
         self.dir_files = []
+        self.dir_files.append("..")
         for element in os.listdir(os.getcwd()):
             fullpath = os.path.join(os.getcwd(),element)
             if os.path.isdir(fullpath):
                 self.dir_files.append(element)
-        self.dir_files.append("..")
         self.dir_files.append("Reached directory")
         self.window.show_quick_panel(self.dir_files, self.handle_change_directory,  sublime.MONOSPACE_FONT)
-
 
     def handle_change_directory(self, call_value):
         if call_value != 1:
             if self.dir_files[call_value] != "Reached directory":
                 os.chdir(self.dir_files[call_value])
                 self.change_directory()
-
 
     def open_new_file(self):
         self.dir_files = []
@@ -47,17 +35,17 @@ class QuickOpenCommand(sublime_plugin.WindowCommand):
                 self.dir_files.append(element)
         self.window.show_quick_panel(self.dir_files, self.handle_open_new_file,  sublime.MONOSPACE_FONT)
 
-
     def handle_open_new_file(self, call_value):
         if call_value != -1:
             self.window.open_file(os.path.join(os.getcwd(), self.dir_files[call_value]))
-
 
     def set_working_directory(self):
         self.window.show_input_panel("Set directory", "", self.handle_set_working_directory, None, None)
 
     def handle_set_working_directory(self, new_dir):
-        print new_dir
+        if new_dir[0] == "~":
+            new_dir = os.getenv("HOME") + new_dir[1:]
+
         try:
             os.chdir(new_dir)
         except:
