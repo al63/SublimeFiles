af583009eb70f6bd8ef2fb9f3374102e5b86a276
Alec Lee <alec.lee15@gmail.com>
2012-03-20 04:25:41 -0400

    actually works now

--- quick_open.py
+++ quick_open.py
@@ -3,10 +3,10 @@ import os
 import glob
 
 
-class QuickOpen(sublime_plugin.WindowCommand):
+class QuickOpenCommand(sublime_plugin.WindowCommand):
 
     def run(self):
-        self.options = ["edit", "change directory"]
+        self.options = ["Edit file", "Change directory", "Set working directory"]
         self.show_commands()
 
     def show_commands(self):
@@ -15,42 +15,54 @@ class QuickOpen(sublime_plugin.WindowCommand):
 
     def choose_command(self, call_value):
         self.command = self.options[call_value]
-        if self.command == "change directory":
-            self.cur_path = os.getenv("HOME")
+        if self.command == "Change directory":
             self.change_directory()
-        else:
-            try:
-                self.cur_path
-            except:
-                self.cur_path = os.getenv("HOME")
+        elif self.command == "Edit file":
             self.open_new_file()
+        else:
+            self.set_working_directory()
 
 
     def change_directory(self):
         self.dir_files = []
-        for element in os.listdir(self.cur_path):
-            fullpath = os.path.join(self.cur_path,element)
+        for element in os.listdir(os.getcwd()):
+            fullpath = os.path.join(os.getcwd(),element)
             if os.path.isdir(fullpath):
                 self.dir_files.append(element)
-        self.dir_files.append("REACHED DIR")
+        self.dir_files.append("..")
+        self.dir_files.append("Reached directory")
         self.window.show_quick_panel(self.dir_files, self.handle_change_directory,  sublime.MONOSPACE_FONT)
 
 
     def handle_change_directory(self, call_value):
         if call_value != 1:
-            if self.dir_files[call_value] != "REACHED DIR":
-                self.cur_path += "/" + self.dir_files[call_value]
+            if self.dir_files[call_value] != "Reached directory":
+                os.chdir(self.dir_files[call_value])
                 self.change_directory()
 
 
     def open_new_file(self):
         self.dir_files = []
-        for element in os.listdir(self.cur_path):
-            fullpath = os.path.join(self.cur_path, element)
+        for element in os.listdir(os.getcwd()):
+            fullpath = os.path.join(os.getcwd(), element)
             if not os.path.isdir(fullpath):
                 self.dir_files.append(element)
         self.window.show_quick_panel(self.dir_files, self.handle_open_new_file,  sublime.MONOSPACE_FONT)
 
 
     def handle_open_new_file(self, call_value):
-        self.window.open_file(self.cur_path + "/" + self.dir_files[call_value])
+        if call_value != -1:
+            self.window.open_file(os.path.join(os.getcwd(), self.dir_files[call_value]))
+
+
+    def set_working_directory(self):
+        self.window.show_input_panel("Set directory", "", self.handle_set_working_directory, None, None)
+
+    def handle_set_working_directory(self, new_dir):
+        print new_dir
+        try:
+            os.chdir(new_dir)
+        except:
+            pass
+
+
