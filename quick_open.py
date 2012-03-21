07f38e605d4594fff984e00a3685b995279e1a81
Alec Lee <alec.lee15@gmail.com>
2012-03-21 05:00:43 -0400

    sorted output

--- quick_open.py
+++ quick_open.py
@@ -10,25 +10,15 @@ class QuickOpenCommand(sublime_plugin.WindowCommand):
 
     #function for showing panel for changing directories / opening files
     def open_navigator(self):
-        dot_files, dot_directories, directories, files, self.dir_files = [], [], [], [], []
-
-        self.dir_files.append(". (" + os.getcwd() + ")")
-        self.dir_files.append("..")
+        self.dir_files = [". (" + os.getcwd() + ")", ".."]
         for element in os.listdir(os.getcwd()):
             fullpath = os.path.join(os.getcwd(),element)
             if os.path.isdir(fullpath):
-                if element[0] == ".":
-                    dot_directories.append(element + "/")
-                else:
-                    directories.append(element+ "/")
+                self.dir_files.append(element + "/")
             else:
-                if element[0] == ".":
-                    dot_files.append(element)
-                else:
-                    files.append(element)
-
-        self.dir_files += files + directories + dot_files + dot_directories
-        self.window.show_quick_panel(self.dir_files, self.handle_select_option,  sublime.MONOSPACE_FONT)
+                self.dir_files.append(element)
+        self.dir_files = sorted(self.dir_files, key=sort_files)
+        self.window.show_quick_panel(self.dir_files, self.handle_select_option, sublime.MONOSPACE_FONT)
 
     #handles user's selection in open_navigator. Either cd's into new directory, or opens file
     def handle_select_option(self, call_value):
@@ -42,13 +32,27 @@ class QuickOpenCommand(sublime_plugin.WindowCommand):
 
     #function for changing the current directory 
     def set_working_directory(self):
-        self.window.show_input_panel("Set directory", "", self.handle_set_working_directory, None, None)
+        self.window.show_input_panel("Set Directory", os.getcwd(), self.handle_set_working_directory, None, None)
 
     #handles changing the directory based on user input
     def handle_set_working_directory(self, new_dir):
-        if new_dir[0] == "~":
-            new_dir = os.getenv("HOME") + new_dir[1:]
         try:
-            os.chdir(new_dir)
+            if new_dir[0] == "~":
+                new_dir = os.getenv("HOME") + new_dir[1:]
+                os.chdir(new_dir)
         except:
-            pass
+            sublime.error_message(new_dir + " does not exist")
+
+
+def sort_files(filename):
+    if filename[0:3] == ". (":
+        return 0
+    if filename == "..":
+        return 1
+    
+    total_weight = 2
+    if filename[0] == ".":
+        total_weight += 2
+    if filename[-1] == "/":
+        total_weight += 1
+    return total_weight
