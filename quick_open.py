b7820f303153c3fec97f150635cc79e0d1aea0b1
Alec Lee <alec.lee15@gmail.com>
2012-03-20 20:59:47 -0400

    updated, changed to only two commands

--- quick_open.py
+++ quick_open.py
@@ -4,39 +4,43 @@ import sys, os
 class QuickOpenCommand(sublime_plugin.WindowCommand):
     def run(self, command):
         if command == "open":
-            self.open_new_file()
-        elif command == "cd":
-            self.change_directory()
+            self.open_navigator()
         elif command == "setdir":
             self.set_working_directory()
 
-    def change_directory(self):
+    def open_navigator(self):
+        dot_files = []
+        dot_directories = []
+        directories = []
+        files = []
         self.dir_files = []
+        
         self.dir_files.append(". (" + os.getcwd() + ")")
         self.dir_files.append("..")
         for element in os.listdir(os.getcwd()):
             fullpath = os.path.join(os.getcwd(),element)
             if os.path.isdir(fullpath):
-                self.dir_files.append(element)
-        self.window.show_quick_panel(self.dir_files, self.handle_change_directory,  sublime.MONOSPACE_FONT)
+                if element[0] == ".":
+                    dot_directories.append(element + "/")
+                else:
+                    directories.append(element+ "/")
+            else:
+                if element[0] == ".":
+                    dot_files.append(element + "/")
+                else:
+                    files.append(element)
 
-    def handle_change_directory(self, call_value):
-        print call_value
+        self.dir_files += files + directories + dot_files + dot_directories
+        self.window.show_quick_panel(self.dir_files, self.handle_select_option,  sublime.MONOSPACE_FONT)
+
+    def handle_select_option(self, call_value):
         if call_value != -1 and call_value != 0:
-            os.chdir(self.dir_files[call_value])
-            self.change_directory()
-
-    def open_new_file(self):
-        self.dir_files = []
-        for element in os.listdir(os.getcwd()):
-            fullpath = os.path.join(os.getcwd(), element)
-            if not os.path.isdir(fullpath):
-                self.dir_files.append(element)
-        self.window.show_quick_panel(self.dir_files, self.handle_open_new_file,  sublime.MONOSPACE_FONT)
-
-    def handle_open_new_file(self, call_value):
-        if call_value != -1:
-            self.window.open_file(os.path.join(os.getcwd(), self.dir_files[call_value]))
+            fullpath = os.path.join(os.getcwd(),self.dir_files[call_value])
+            if os.path.isdir(fullpath):
+                os.chdir(self.dir_files[call_value])
+                self.open_navigator()
+            else:
+                self.window.open_file(os.path.join(os.getcwd(), self.dir_files[call_value]))
 
     def set_working_directory(self):
         self.window.show_input_panel("Set directory", "", self.handle_set_working_directory, None, None)
